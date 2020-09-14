bf922b79c2a7821e57cd98d64e5b35c7687b758e
komakkar <komakkar@microsoft.com>
2020-09-14 10:37:47 +0530

    EG changes

--- provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/MessageBusImpl.java
+++ provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/MessageBusImpl.java
@@ -16,20 +16,26 @@ package org.opengroup.osdu.storage.provider.azure;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.microsoft.azure.eventgrid.implementation.EventGridClientImpl;
+import com.microsoft.azure.eventgrid.models.EventGridEvent;
 import com.microsoft.azure.servicebus.Message;
 import com.microsoft.azure.servicebus.TopicClient;
+import com.sun.jna.platform.win32.Guid;
+import com.sun.jna.platform.win32.Guid.GUID;
+import org.joda.time.DateTime;
 import org.opengroup.osdu.core.common.model.storage.PubSubInfo;
 import org.opengroup.osdu.core.common.logging.JaxRsDpsLog;
 import org.opengroup.osdu.storage.provider.interfaces.IMessageBus;
 import org.opengroup.osdu.core.common.model.http.DpsHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-//import com.microsoft.azure.eventgrid.EventGridClient;
-//import com.microsoft.azure.eventgrid.TopicCredentials;
+import com.microsoft.azure.eventgrid.EventGridClient;
+import com.microsoft.azure.eventgrid.TopicCredentials;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class MessageBusImpl implements IMessageBus {
@@ -39,6 +45,7 @@ public class MessageBusImpl implements IMessageBus {
 
     @Autowired
     private JaxRsDpsLog logger;
+
 
     @Override
     public void publishMessage(DpsHeaders headers, PubSubInfo... messages) {
@@ -82,12 +89,14 @@ public class MessageBusImpl implements IMessageBus {
         }
     }
 
-    public void publishMessages(DpsHeaders headers, PubSubInfo... messages) {
+
+    /*@Override
+    public void publishMessage(DpsHeaders headers, PubSubInfo... messages)  {
         final int BATCH_SIZE = 50;
         Gson gson = new Gson();
 
         for (int i = 0; i < messages.length; i += BATCH_SIZE) {
-            Message message = new Message();
+
             Map<String, Object> properties = new HashMap<>();
 
             // properties
@@ -95,7 +104,7 @@ public class MessageBusImpl implements IMessageBus {
             properties.put(DpsHeaders.DATA_PARTITION_ID, headers.getPartitionIdWithFallbackToAccountId());
             headers.addCorrelationIdIfMissing();
             properties.put(DpsHeaders.CORRELATION_ID, headers.getCorrelationId());
-            message.setProperties(properties);
+            //message.setProperties(properties);
 
             // data
             PubSubInfo[] batch = Arrays.copyOfRange(messages, i, Math.min(messages.length, i + BATCH_SIZE));
@@ -109,23 +118,40 @@ public class MessageBusImpl implements IMessageBus {
             JsonObject jomsg = new JsonObject();
             jomsg.add("message", jo);
 
-            message.setBody(jomsg.toString().getBytes(StandardCharsets.UTF_8));
-            message.setContentType("application/json");
-  /*          EventGridClient client = new EventGridClientImpl(topicCredentials);
-            String eventGridEndpoint = String.format("https://%s/", new URI(System.getenv("EVENTGRID_TOPIC_ENDPOINT")).getHost());
+            //message.setBody(jomsg.toString().getBytes(StandardCharsets.UTF_8));
+            //message.setContentType("application/json");
+            TopicCredentials topicCredentials = new TopicCredentials("jUrTK5wrk3rozBjWkZFlLr45DqL5zsTN+6MYtge9l94=");
+            //https://github.com/Azure-Samples/event-grid-java-publish-consume-events/blob/master/eventgrid-function-apps-producer-consumer/pom.xml
+            EventGridClient client = new EventGridClientImpl(topicCredentials);
+            String eventGridEndpoint = null;
+            try {
+                eventGridEndpoint = String.format("https://%s/", new URI("https://recordchanged.westus2-1.eventgrid.azure.net/api/events").getHost());
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+            //"https://recordchanged.westus2-1.eventgrid.azure.net/api/events";
 
-            client.publishEvents(eventGridEndpoint, eventsList);
-*/
+            List<EventGridEvent> eventsList = new ArrayList<>();
+            for (int j = 0; j < 5; j++) {
+                eventsList.add(new EventGridEvent(
+                        UUID.randomUUID().toString(),
+                        String.format("Door%d", i),
+                        jomsg,
+                        "Contoso.Items.ItemReceived",
+                        DateTime.now(),
+                        "2.0"
+                ));
+            }
 
             try {
                 logger.info("Storage publishes message " + headers.getCorrelationId());
-                topicClient.send(message);
+                //topicClient.send(message);
+                client.publishEvents(eventGridEndpoint, eventsList);
             }
             catch (Exception e)
             {
                 logger.error(e.getMessage(), e);
             }
         }
-    }
-
+    }*/
 }
