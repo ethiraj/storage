70090fe096529d3e2dfc5af6782bacea9e0ef921
Vibhuti Sharma <vibsharm@microsoft.com>
2020-10-30 13:33:13 +0530

    added logic to check bindings

--- provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/StorageApplication.java
+++ provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/StorageApplication.java
@@ -14,10 +14,18 @@
 
 package org.opengroup.osdu.storage.provider.azure;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.helpers.Util;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 /**
  * Note: these exclusions are the result of duplicate dependencies being introduced in the
@@ -33,7 +41,53 @@ import org.springframework.context.annotation.FilterType;
 )
 @SpringBootApplication
 public class StorageApplication {
+
+    // Find all resources on classpath with the name "org/slf4j/impl/StaticLoggerBinder.class"
+    // If there are multiple resources, that means there are multiple slf4j bindings present
+    // This will break binding with log4j2 and consequently it will break the integration with appinsights
+    // Thus, if multiple bindings are present, then we do not run the application
+    private static final String STATIC_LOGGER_BINDER_PATH = "org/slf4j/impl/StaticLoggerBinder.class";
+    private static final String MULTIPLE_BINDINGS_URL = "http://www.slf4j.org/codes.html#StaticLoggerBinder";
+    private static Set<URL> staticLoggerBinderPathSet = new LinkedHashSet<URL>();
+
     public static void main(String[] args) {
-        SpringApplication.run(StorageApplication.class, args);
+        staticLoggerBinderPathSet = findPossibleStaticLoggerBinderPathSet();
+        if(!multipleBindingPresent(staticLoggerBinderPathSet)) {
+            SpringApplication.run(StorageApplication.class, args);
+        }
+        else {
+            throw new RuntimeException("APPLICATION FAILED TO START");
+        }
+    }
+
+    private static Set<URL> findPossibleStaticLoggerBinderPathSet() {
+        try {
+            ClassLoader loggerFactoryClassLoader = LoggerFactory.class.getClassLoader();
+            Enumeration<URL> paths;
+            if (loggerFactoryClassLoader == null) {
+                paths = ClassLoader.getSystemResources(STATIC_LOGGER_BINDER_PATH);
+            } else {
+                paths = loggerFactoryClassLoader.getResources(STATIC_LOGGER_BINDER_PATH);
+            }
+            while (paths.hasMoreElements()) {
+                URL path = paths.nextElement();
+                staticLoggerBinderPathSet.add(path);
+            }
+        } catch (IOException ioe) {
+            Util.report("Error getting resources from path", ioe);
+        }
+        return staticLoggerBinderPathSet;
+    }
+
+    private static boolean multipleBindingPresent(Set<URL> binderPathSet) {
+        if (binderPathSet.size() > 1) {
+            Util.report("Class path contains multiple SLF4J bindings.");
+            for (URL path : binderPathSet) {
+                Util.report("Found binding in [" + path + "]");
+            }
+            Util.report("See " + MULTIPLE_BINDINGS_URL + " for an explanation.");
+            return true;
+        }
+        return false;
     }
 }
