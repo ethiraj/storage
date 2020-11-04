8f0d49c3cde2b03890a163bc0f6663e653647302
a_darafeyeu <ADarafeyeu@slb.com>
2020-11-04 13:42:07 +0300

    gets redis-host

--- provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/di/cache/RedisConfig.java
+++ provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/di/cache/RedisConfig.java
@@ -1,5 +1,8 @@
 package org.opengroup.osdu.storage.provider.azure.di.cache;
 
+import javax.inject.Named;
+
+import org.opengroup.osdu.azure.KeyVaultFacade;
 import org.opengroup.osdu.core.common.cache.RedisCache;
 import org.opengroup.osdu.core.common.model.entitlements.Groups;
 import org.opengroup.osdu.core.common.model.storage.Schema;
@@ -8,15 +11,26 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.azure.security.keyvault.secrets.SecretClient;
+
 @Configuration
 public class RedisConfig {
+
+  @Value("${redis.host}")
+  private String redisHost;
+
+  @Bean
+  @Named("REDIS_HOST")
+  public String redisHost(SecretClient kv) {
+    if (redisHost == null || redisHost.isEmpty()) {
+      redisHost = KeyVaultFacade.getSecretWithValidation(kv, "redis-hostname");
+    }
+    return redisHost;
+  }
 
   @Configuration
   @ConditionalOnExpression(value = "'${cache.provider}' == 'redis' && '${redis.ssl.enabled:false}'")
   static class SslConfig {
-
-    @Value("${redis.host}")
-    private String host;
 
     @Value("${redis.port}")
     private int port;
@@ -31,17 +45,17 @@ public class RedisConfig {
     private String password;
 
     @Bean
-    public RedisCache<String, Groups> groupCache() {
+    public RedisCache<String, Groups> groupCache(@Named("REDIS_HOST") String host) {
       return new RedisCache<>(host, port, password, expiration, database, String.class, Groups.class);
     }
 
     @Bean("legalTagCache")
-    public RedisCache<String, String> legalTagCache() {
+    public RedisCache<String, String> legalTagCache(@Named("REDIS_HOST") String host) {
       return new RedisCache<>(host, port, password, expiration, database, String.class, String.class);
     }
 
     @Bean
-    public RedisCache<String, Schema> schemaCache() {
+    public RedisCache<String, Schema> schemaCache(@Named("REDIS_HOST") String host) {
       return new RedisCache<>(host, port, password, expiration, database, String.class, Schema.class);
     }
   }
@@ -49,9 +63,6 @@ public class RedisConfig {
   @Configuration
   @ConditionalOnExpression(value = "'${cache.provider}' == 'redis' && !'${redis.ssl.enabled:true}'")
   static class NoSslConfig {
-
-    @Value("${redis.host}")
-    private String host;
 
     @Value("${redis.port}")
     private int port;
@@ -63,17 +74,17 @@ public class RedisConfig {
     private int expiration;
 
     @Bean
-    public RedisCache<String, Groups> groupCache() {
+    public RedisCache<String, Groups> groupCache(@Named("REDIS_HOST") String host) {
       return new RedisCache<>(host, port, expiration, database, String.class, Groups.class);
     }
 
     @Bean("legalTagCache")
-    public RedisCache<String, String> legalTagCache() {
+    public RedisCache<String, String> legalTagCache(@Named("REDIS_HOST") String host) {
       return new RedisCache<>(host, port, expiration, database, String.class, String.class);
     }
 
     @Bean
-    public RedisCache<String, Schema> schemaCache() {
+    public RedisCache<String, Schema> schemaCache(@Named("REDIS_HOST") String host) {
       return new RedisCache<>(host, port, expiration, database, String.class, Schema.class);
     }
   }
