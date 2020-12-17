c2dd87e0f4a9d0c80d1323116bb1bbac39e141f5
komakkar <komakkar@microsoft.com>
2020-12-17 12:51:14 +0530

    intermediate changes

--- provider/storage-azure/src/test/java/org/opengroup/osdu/storage/provider/azure/MessageBusImplTest.java
+++ provider/storage-azure/src/test/java/org/opengroup/osdu/storage/provider/azure/MessageBusImplTest.java
@@ -1,3 +1,4 @@
+/*
 // Copyright © Microsoft Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,16 +20,13 @@ import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.opengroup.osdu.azure.eventgrid.EventGridTopicStore;
 import org.opengroup.osdu.azure.eventgrid.TopicName;
@@ -49,8 +47,6 @@ import static org.mockito.MockitoAnnotations.initMocks;
 
 
 @RunWith(MockitoJUnitRunner.class)
-
-@ExtendWith(MockitoExtension.class)
 public class MessageBusImplTest {
     
     private static final String DATA_PARTITION_WITH_FALLBACK_ACCOUNT_ID = "data-partition-account-id";
@@ -59,9 +55,11 @@ public class MessageBusImplTest {
 
     @Mock
     private ITopicClientFactory topicClientFactory;
-    /*@Mock
+    */
+/*@Mock
     private TopicClient topicClient;
-*/
+*//*
+
 
     @Mock
     private EventGridTopicStore eventGridTopicStore;
@@ -76,19 +74,21 @@ public class MessageBusImplTest {
     private MessageBusImpl sut;
 
     @Before
-    public void init() throws ServiceBusException, InterruptedException {
-        initMocks(this);
+    public void init()  {
+        MockitoAnnotations.initMocks(this);
 
        // TopicClient concreteTopicClient = new TopicClient(new ConnectionStringBuilder("connectionString"));
 
-        TopicClient mocktopicClient = mock(TopicClient.class);
+        //TopicClient mocktopicClient = mock(TopicClient.class);
 
 
 
-   doReturn(DATA_PARTITION_WITH_FALLBACK_ACCOUNT_ID).when(dpsHeaders).getPartitionIdWithFallbackToAccountId();
+        */
+/*doReturn(DATA_PARTITION_WITH_FALLBACK_ACCOUNT_ID).when(dpsHeaders).getPartitionIdWithFallbackToAccountId();
         doReturn(PARTITION_ID).when(dpsHeaders).getPartitionId();
         doReturn(CORRELATION_ID).when(dpsHeaders).getCorrelationId();
-        doReturn(mocktopicClient).when(topicClientFactory).getClient(eq(PARTITION_ID), any());
+        doReturn(mocktopicClient).when(topicClientFactory).getClient(eq(PARTITION_ID), any());*//*
+
     }
 
 
@@ -119,3 +119,4 @@ public class MessageBusImplTest {
         return pubSubInfo;
     }
 }
+*/
