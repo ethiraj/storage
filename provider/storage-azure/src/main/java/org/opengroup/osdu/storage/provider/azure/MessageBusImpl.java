d8952094ab187812ef3877309a1cd34ee350fc21
Alok Joshi <ajoshi19@slb.com>
2020-12-22 15:45:25 -0600

    log message to messageBus

--- provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/MessageBusImpl.java
+++ provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/MessageBusImpl.java
@@ -71,6 +71,7 @@ public class MessageBusImpl implements IMessageBus {
             jo.addProperty(DpsHeaders.CORRELATION_ID, headers.getCorrelationId());
             JsonObject jomsg = new JsonObject();
             jomsg.add("message", jo);
+            logger.info("Message passed to Indexer-queue: "+jo.getAsString());
 
             message.setBody(jomsg.toString().getBytes(StandardCharsets.UTF_8));
             message.setContentType("application/json");
