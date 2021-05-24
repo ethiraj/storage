c18c6be0463a3567925e808b656b8fcb24e69ecf
NThakur4 <nthakur4@slb.com>
2021-05-24 16:08:51 -0500

    revert commit e8609f41

--- provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/util/RecordIdValidator.java
+++ provider/storage-azure/src/main/java/org/opengroup/osdu/storage/provider/azure/util/RecordIdValidator.java
@@ -2,7 +2,6 @@ package org.opengroup.osdu.storage.provider.azure.util;
 
 import org.apache.http.HttpStatus;
 import org.opengroup.osdu.core.common.model.http.AppException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,15 +10,9 @@ import java.util.Objects;
 @Component
 public class RecordIdValidator {
 
-    private final int recordIdMaxLength;
-
-    public RecordIdValidator(@Value("${record-id.max.length}") int recordIdMaxLength) {
-        this.recordIdMaxLength = recordIdMaxLength;
-    }
-
     public void validateIds(List<String> inputRecords) {
         if (inputRecords.stream().filter(Objects::nonNull)
-                .anyMatch(id -> id.length() > recordIdMaxLength)) {
+                .anyMatch(id -> id.length() > 100)) {
             String msg = "RecordId values which are exceeded 100 symbols temporarily not allowed";
             throw new AppException(HttpStatus.SC_BAD_REQUEST, "Invalid id", msg);
         }
